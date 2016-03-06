f8db853c05a3c81255166edb8e5aa11dbd9c580b
fsoeduardo <fsoeduardo@gmail.com>
2016-03-06 20:47:35 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,86 @@
+import java.awt.EventQueue;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class Main {
+
+	private JFrame frame;
+	private Point clicked;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setUndecorated(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		frame.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				clicked = e.getPoint();
+	            frame.getComponentAt(clicked);
+			}
+		});
+		
+		frame.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// get location of Window
+	            int thisX = frame.getLocation().x;
+	            int thisY = frame.getLocation().y;
+
+	            // Determine how much the mouse moved since the initial click
+	            int xMoved = (thisX + e.getX()) - (thisX + clicked.x);
+	            int yMoved = (thisY + e.getY()) - (thisY + clicked.y);
+
+	            // Move window to this position
+	            int X = thisX + xMoved;
+	            int Y = thisY + yMoved;
+	            frame.setLocation(X, Y);
+			}
+		});
+		panel.setBounds(0, 0, 450, 300);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel(":D");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 26));
+		lblNewLabel.setBounds(10, 87, 430, 33);
+		panel.add(lblNewLabel);
+	}
+}
